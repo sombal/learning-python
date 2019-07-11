e3cfc446ed8213948c36fd4a7801d5194ea468a8
Balazs Somogyi <balazs.somogyi@t-systems.com>
2019-07-11 12:31:19 +0200

    change find-primes.py

--- find-primes.py
+++ find-primes.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 startTime = datetime.now()
 primes = []
-for n in range(2, 10001):
+for n in range(2, 100001):
     for x in range(2, n):
         if n % x == 0:
-            print(n, 'equals', x, '*', n//x)
+            #print(n, 'equals', x, '*', n//x)
             break
     else:
         # loop fell through without finding a factor
-        print(n, 'is prime')
+        #print(n, 'is prime')
         primes.append(n)
 
 print("Biggest prime in range:", primes[-1])
