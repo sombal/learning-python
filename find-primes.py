37c8ade2bbfbbc82795d515bf87349caa941ae66
Balazs Somogyi <balazs.somogyi@t-systems.com>
2019-09-27 23:50:29 +0200

    add from pycharm

--- find-primes.py
+++ find-primes.py
@@ -1,16 +1,17 @@
 from datetime import datetime
+
 startTime = datetime.now()
 primes = []
 for n in range(2, 10001):
     for x in range(2, n):
         if n % x == 0:
-            #print(n, 'equals', x, '*', n//x)
+            # print(n, 'equals', x, '*', n//x)
             break
     else:
         # loop fell through without finding a factor
-        #print(n, 'is prime')
+        # print(n, 'is prime')
         primes.append(n)
 
 print("Biggest prime in range:", primes[-1])
 endTime = datetime.now()
-print("Total runtime: " + str(endTime-startTime))
+print("Total runtime: " + str(endTime - startTime))
