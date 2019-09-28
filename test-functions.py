d392353f27a9ce1745aa10d811227c48b9ce5186
Balazs Somogyi <balazs.somogyi@t-systems.com>
2019-09-28 21:58:35 +0200

    update code

--- test-functions.py
+++ test-functions.py
@@ -5,11 +5,13 @@ clientId = "649122f0-859f-487c-ad09-75528470c6df"
 clientSecret = "nofAneTFsGyr/z/d98Ul9sFM+aJ4myLmWg7Lg3cDdgg="
 tenantId = "3d08d325-47e1-4822-89e2-07ec9997b68e"
 
+
 # Proxy in case in corp network
 def use_proxy():
     proxy = 'http://aht:MindenFasza69@10.220.40.138:3128'
     os.environ['http_proxy'] = proxy
     os.environ['https_proxy'] = proxy
+
 
 # Getting authentication header
 requestHeader = {}
@@ -34,6 +36,7 @@ subId = "19625c0d-007e-48a2-8e0d-e64f68a02ce0"
 vmLocation = "westeurope"
 uriPrefix = "https://management.azure.com/subscriptions/"
 
-uri = str(uriPrefix + subId + "/resourceGroups/" + rgpName + "/providers/Microsoft.Compute/virtualMachines/" + vmName + "/extensions/joindomain?api-version=2018-06-01")
+uri = str(
+    uriPrefix + subId + "/resourceGroups/" + rgpName + "/providers/Microsoft.Compute/virtualMachines/" + vmName + "/extensions/joindomain?api-version=2018-06-01")
 extCheck = (requests.get(uri, headers=requestHeader))
 print(extCheck.json().get("properties").get("provisioningState"))
